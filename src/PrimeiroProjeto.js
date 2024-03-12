8a78df5d27f3ca7f66b6d429c26df9479179adb7
Gabriela Sozzo <143351518+GabrielaSozzo@users.noreply.github.com>
2024-03-12 12:03:06 -0300

    tarefa realizada

--- src/PrimeiroProjeto.js
+++ src/PrimeiroProjeto.js
@@ -1,1 +1,39 @@
-console.log("Olá! Como vai?")
+let nomeUsuario = "Gabriela"
+let xpUsuario = 100000
+let nivelUsuario = ""
+
+if (xpUsuario <= 1000) {
+  nivelUsuario = "Ferro"
+// Menor ou igual a
+
+} else if (xpUsuario <= 2000) {
+  nivelUsuario = "Bronze"
+// Menor ou igual a
+
+} else if (xpUsuario <= 6000) {
+  nivelUsuario = "Prata"
+// Menor ou igual a
+
+} else if (xpUsuario <= 7000) {
+  nivelUsuario = "Ouro"
+// Menor ou igual a
+
+} else if (xpUsuario <= 8000) {
+    nivelUsuario = "Platina"
+// Menor ou igual a
+
+} else if (xpUsuario <= 9000) {
+  nivelUsuario = "Ascendente"
+// Menor ou igual a
+
+} else if (xpUsuario <= 10000) {
+  nivelUsuario = "Imortal"
+// Menor ou igual a
+
+} else if (xpUsuario >= 10001) {
+  nivelUsuario = "Radiante"
+// Maior ou igual a
+
+}
+
+console.log("O Herói de nome " + nomeUsuario + " está no nível de " + nivelUsuario)
